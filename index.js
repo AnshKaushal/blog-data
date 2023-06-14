68ac445ba852b681c8716ba6137a07177009121c
Anshh Kaushal <ansh1043.be21@chitkarauniversity.edu.in>
2023-06-14 19:35:23 +0530

    add new route

--- index.js
+++ index.js
@@ -9,16 +9,39 @@ const port = process.env.PORT || 3000
 
 const connection = mysql.createConnection(process.env.DATABASE_URL)
 
+// Getting all posts
 app.get('/api/data', (req, res) => {
     connection.query('SELECT * FROM  posts', (error, results) => {
       if (error) {
         console.error('Error fetching data:', error);
         res.status(500).json({ error: 'Internal server error' });
       } else {
-        res.json(results);
+        res.json({posts : results});
       }
     });
+});
+
+// Getting individual posts
+app.get('/api/post', (req, res) => {
+  const postId = req.query.post;
+
+  if (!postId) {
+    res.status(400).json({ error: 'Post ID is required' });
+    return;
+  }
+
+  connection.query('SELECT * FROM posts WHERE id = ?', [postId], (error, results) => {
+    if (error) {
+      console.error('Error fetching data:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    } else if (results.length === 0) {
+      res.status(404).json({ error: 'Post not found' });
+    } else {
+      const post = results[0];
+      res.json({ post });
+    }
   });
+});
 
   app.listen(port, () => {
     console.log(`Open http://localhost:3000`);
