b6551f2fa2920c209e788d00e748c07336d910b8
Anshh Kaushal <ansh1043.be21@chitkarauniversity.edu.in>
2023-06-14 18:43:40 +0530

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const mysql = require('mysql2')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 const port = process.env.PORT || 3000
 
 const connection = mysql.createConnection(process.env.DATABASE_URL)
